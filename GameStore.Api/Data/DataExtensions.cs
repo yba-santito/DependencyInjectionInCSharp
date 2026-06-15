30be7b70efa4c6fae7d73de2a2b397cb2b2de624
yba-santito <odsctradinggroup@gmail.com>
2026-06-15 20:50:04 +0200

    Learning on migrating the database and using nuGet packages that are handy ins such a transition for C# deveopment, created Models and cleaned up the program.cs, following f the SOLID principles

---
+++ GameStore.Api/Data/DataExtensions.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Api.Data
+{
+    public static class DataExtensions
+    {
+        public static void MigrateDb(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.
+                        GetRequiredService<GameStoreContext>();
+    
+            dbContext.Database.Migrate();
+    }
+    }
+};
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,11 +1,18 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
 
+var connString = "Data Source=GameStore.db";
+
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
+
+app.MigrateDb();
 
 app.Run();
